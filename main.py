fe79d829be563ff5d8f175fe2a019fe792139b69
Elijah Arsenault <152457127+ElijahArsenault@users.noreply.github.com>
2024-02-13 16:54:40 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+import random
+
+# Assigning characters to piece variables
+queen = "Q"
+king = "K"
+rook1 = "R"
+rook2 = "r"
+bishop_light = "B"
+bishop_dark = "b"
+knight1 = "n"
+knight2 = "N"
+
+
+# This function is used to ensure the bishops are on opposite colors
+def is_even(x):
+    if x % 2 == 0:
+        return True
+    else:
+        return False
+
+
+# Loop to keep trying combinations until a legal position is reached
+while True:
+    position = [queen, king, rook1, rook2, bishop_light, bishop_dark, knight1, knight2]
+    random.shuffle(position)
+    # To insure bishops are on opposite colors
+    if (position.index(bishop_dark) - position.index(bishop_light)) % 2 == 0:
+        continue
+    # Checks if king is between rooks
+    elif position.index(rook1) > position.index(king) and position.index(rook2) > position.index(king):
+        continue
+    elif position.index(rook1) < position.index(king) and position.index(rook2) < position.index(king):
+        continue
+    else:
+        break
+
+position = str(position)
+print("Chess960 position:")
+# Prints position in a more readable way
+print(position.strip("[").strip("]").replace("'", "").replace(",", ""))
